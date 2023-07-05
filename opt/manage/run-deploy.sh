c9cf1e352e8bc2a9d90e3d5759f1999b2562cad2
iLyaSerikovv <iserikov@perfectpanel.com>
2023-07-05 09:36:23 +0000

    1.0.3 commit

--- opt/manage/run-deploy.sh
+++ opt/manage/run-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -vvv -i /opt/manage/etc/ansible/hosts.ini /opt/manage/opt/manage/deploy-stage.yaml
+ansible-playbook -vvv -i /opt/manage/Stage-Setup/etc/ansible/hosts.ini /opt/manage/Stage-Setup/opt/manage/deploy-stage.yaml
